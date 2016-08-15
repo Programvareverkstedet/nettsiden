9653716ff567068c6f985d6270dbc271d8b8aa19
Karl August Hoivik <gussefant@gmail.com>
2016-08-15 20:56:24 +0200

    Get all events from MySQL DB

--- src/pvv/side/events.php
+++ src/pvv/side/events.php
@@ -1,11 +1,19 @@
 <?php
 namespace pvv\side;
+use \mysqli;
+require __DIR__.'/sql_config.php';
 
 Class Events {
     public function getAllEvents(){
+	global $url, $user,$pass,$db;
 	$events = array();
-	$ev = new OnceEvent("Test","Now","Never","sm0xe","Here");
-	$events[0] = $ev;
+	$mysqli = new mysqli($url,$user,$pass,$db);
+	$result = $mysqli->query("SELECT name,start,stop,organiser,location FROM events");
+	while($row = $result->fetch_assoc()){
+	$ev = new OnceEvent($row['name'],$row['start'],$row['stop'],$row['organiser'],$row['location']);
+	array_push($events,$ev);
+	}
+	#array_sort($events);
 	return $events;
     }
 }
---
+++ src/pvv/side/sql_config_example.php
@@ -0,0 +1,6 @@
+<?php
+$url = "127.0.0.1";
+$user = "user";
+$pass = "password";
+$db = "events";
+?>
