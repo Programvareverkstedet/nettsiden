e0385a9fef76bcbd8e69d92b407af6c8f1578af3
Aleksander Wasaznik <aleksanw@stud.ntnu.no>
2016-08-18 00:25:47 +0200

    Autohyphenate <p>
    Add hyphens:auto to <p> to remove the ugly gaps introduced by
    text-align: justified. This may cause some unwanted hyphenation on e.g.
    email addresses. We should strive to not have long unbreakable words.
    
    For record; command for adding html tag to all relevant files:
    $ find -type f -exec sed -i '/DOCTYPE/ a <html lang="no">' {} +

--- www/index.php
+++ www/index.php
@@ -1,4 +1,5 @@
 <?php require '../src/_autoload.php'; ?><!DOCTYPE html>
+<html lang="no">
 <title>Programvareverkstedet</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
