a3c68c3c7932b12e8f7a74e1eefe2a6d0c1ffcf2
johnche <john20chen@gmail.com>
2017-10-07 21:25:44 +0200

    add feide authentication

--- www/paamelding/index.php
+++ www/paamelding/index.php
@@ -1,4 +1,31 @@
 <?php
+require_once '../../lib/OAuth2-Client/OAuth2Client.php';
+require_once '../../dataporten_config.php';
+
+$oauth2 = new Kasperrt\Oauth2($dataportenConfig);
+
+session_start();
+if (isset($_GET['logout'])) {
+	session_destroy();
+	header('Location: http://[::1]:1080/paamelding/');
+	die();
+}
+
+if (isset($_GET['code'])) {
+	$token = $oauth2 -> get_access_token();
+	error_log($token);
+	$_SESSION['userdata'] = $oauth2 -> get_identity($token, 'https://auth.dataporten.no/userinfo');
+	header('Location: http://[::1]:1080/paamelding/');
+	die();
+}
+if (!isset($_SESSION['userdata'])) {
+	$oauth2 -> redirect();
+	die();
+}
+
+//var_export($_SESSION);
+//exit;
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	if($_POST["firstname"] == null &&
 		$_POST["lastname"] == null &&
@@ -35,20 +62,16 @@ die();
 <div>
 	<form>
 		<div>
-			First name:
-			<input type="text" name="firstname">
-		</div>
-		<div>
-			Last name:
-			<input type="text" name="lastname">
+			Full name:
+			<input type="text" name="lastname" value="<?= $_SESSION['userdata']['user']['name'] ?>" readonly>
 		</div>
 		<div>
 			NTNU username:
-			<input type="text" name="username">
+			<input type="text" name="username" value="<?= $_SESSION['userdata']['user']['userid_sec'][0] ?>" readonly>
 		</div>
 		<div>
 			Email adress:
-			<input type="text" name="email">
+			<input type="text" name="email" value="<?= $_SESSION['userdata']['user']['email'] ?>">
 		</div>
 		<input type="submit" value="Submit">
 	</form>
