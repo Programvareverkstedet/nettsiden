731663e4c0880b6f3e01daa1096ac6d813d8f83e
Peder Bergebakken Sundt <pbsds@hotmail.com>
2017-10-22 17:32:54 +0200

    Fixed logout

--- www/login/index.php
+++ www/login/index.php
@@ -2,9 +2,13 @@
 require_once(__DIR__ . '/../../vendor/simplesamlphp/simplesamlphp/lib/_autoload.php');
 $as = new SimpleSAML_Auth_Simple('default-sp');
 $as->requireAuth();
-
 $attributes = $as->getAttributes();
-print_r($attributes);
 ?>
 
-test
+<p>
+	Username: <?= $attributes["uid"][0]?><br>
+	Full name: <?= $attributes["cn"][0]?><br>
+	Email: <?= $attributes["mail"][0]?><br>
+</p>
+
+<a href="<?= htmlspecialchars($as->getLogoutURL("http://localhost:1080/")) ?>">logout</a>
