89ce6773a866a4922c3c1a3b158c0983eeda0581
halworsen <mwh@halvorsenfamilien.com>
2018-02-13 15:50:45 +0100

    Redo landing page main body MOTD is WIP

--- www/index.php
+++ www/index.php
@@ -59,53 +59,50 @@ $agenda = new \pvv\side\Agenda([
 		</div>
 	</header>
 
-	<main>
-		<h2>Kommende arrangement</h2>
-		<ul class="calendar-events">
-			<?php $counter1 = 0; ?>
-			<?php $counter2 = 0; ?>
-			<?php foreach($agenda->getNextDays() as $period => $events) if ($events && $counter1 < 2 && $counter2 < 10) { $counter1++ ?>
-			<li>
-				<p><?= $translation[$period] ?></p>
-				<ul>
-					<?php foreach($events as $event) { $counter2++ ?>
-					<li>
-						<a href="<?= htmlspecialchars($event->getURL()) ?>"><?= $event->getName(); ?></a>
-						<?php /* <a class="icon subscribe">+</a> */ ?>
-						<?php if ($period !== \pvv\side\Agenda::TODAY) {
-							echo '<span class="time">' . $event->getStart()->format('H:i') . '</span>';
-							if (\pvv\side\Agenda::isThisWeek($event->getStart()) || $event->getStart()->sub(new DateInterval('P3D'))->getTimestamp() < time()) {
-								echo '<span class="date">' . strftime('%a', $event->getStart()->getTimestamp()) . '</span>';
+	<main class="contentsplit">
+		<div class="gridr">
+			<h2>Kommende aktiviteter</h2>
+			<div class="calendar-events">
+				<?php $counter1 = 0; ?>
+				<?php $counter2 = 0; ?>
+				<?php foreach($agenda->getNextDays() as $period => $events) if ($events && $counter1 < 3 && $counter2 < 10) { $counter1++ ?>
+					<p class="no-chin"><?= $translation[$period] ?></p>
+					<hr>
+					<ul>
+						<?php foreach($events as $event) { $counter2++ ?>
+							<li>
+							<a href="<?= htmlspecialchars($event->getURL()) ?>"><?= $event->getName(); ?></a>
+							<?php /* <a class="icon subscribe">+</a> */ ?>
+							<?php if ($period !== \pvv\side\Agenda::TODAY) {
+								echo '<span class="time">' . $event->getStart()->format('H:i') . '</span>';
+								if (\pvv\side\Agenda::isThisWeek($event->getStart()) || $event->getStart()->sub(new DateInterval('P3D'))->getTimestamp() < time()) {
+									echo '<span class="date">' . strftime('%a', $event->getStart()->getTimestamp()) . '</span>';
+								} else {
+									echo '<span class="date">' . strftime('%e. %b', $event->getStart()->getTimestamp()) . '</span>';
+								}
 							} else {
-								echo '<span class="date">' . strftime('%e. %b', $event->getStart()->getTimestamp()) . '</span>';
+								echo '<span class="time">' . $event->getStart()->format('H:i') . '</span>';
 							}
-						} else {
-							echo '<span class="time">' . $event->getStart()->format('H:i') . '</span>';
-						}
-						?>
-					</li>
-					<?php } ?>
-				</ul>
-			</li>
-			<?php } ?>
-		</ul>
-		<p><a class="btn" href="kalender/">Flere aktiviteter</a></p>
+							?>
+						</li>
+						<?php } ?>
+					</ul>
+				<?php } ?>
+			</div>
+			<br>
+			<p><a class="btn" href="kalender/">Flere aktiviteter</a></p>
+		</div>
+		
+		<div class="gridl">
+			<h2>Dagens melding</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio tempor orci dapibus ultrices in. Et odio pellentesque diam volutpat commodo. Porttitor leo a diam sollicitudin. Nisl nisi scelerisque eu ultrices. Ipsum dolor sit amet consectetur. Mattis pellentesque id nibh tortor id. Quam adipiscing vitae proin sagittis nisl rhoncus mattis. Integer malesuada nunc vel risus commodo. Curabitur gravida arcu ac tortor dignissim. Mi sit amet mauris commodo quis imperdiet. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Pretium quam vulputate dignissim suspendisse in est. Velit egestas dui id ornare. Urna condimentum mattis pellentesque id nibh tortor id aliquet.<br><br>
 
-		<h2>Opptak</h2>
-		<p>
-		Alle med tilknytning til NTNU kan bli medlem hos oss
-		og benytte seg av våre ressurser.
-		<!--
-		For å bli med i våre prosjekter og komitéer må du søke.
-		-->
-		</p>
-		<p>
-		<a class="btn" href="paamelding/">Bli medlem</a>
-		<!--
-		<a class="btn" href="prosjekt/">Søk prosjekt</a>
-		<a class="btn" href="prosjekt/">Søk komité</a>
-		-->
-		</p>
+Maecenas pharetra convallis posuere morbi leo urna molestie. Egestas sed sed risus pretium. At erat pellentesque adipiscing commodo elit. Ut tortor pretium viverra suspendisse potenti nullam ac. Sit amet volutpat consequat mauris nunc congue nisi vitae suscipit. Faucibus purus in massa tempor nec feugiat nisl. Viverra tellus in hac habitasse platea dictumst vestibulum. Tincidunt vitae semper quis lectus nulla at. Id consectetur purus ut faucibus. Ultricies integer quis auctor elit sed vulputate. Suscipit adipiscing bibendum est ultricies integer quis auctor elit.<br><br>
+
+Nulla malesuada pellentesque elit eget. Odio tempor orci dapibus ultrices in iaculis nunc. Iaculis at erat pellentesque adipiscing. Volutpat ac tincidunt vitae semper. Posuere ac ut consequat semper viverra nam libero justo. Enim tortor at auctor urna nunc id cursus metus. Sit amet cursus sit amet. Eu non diam phasellus vestibulum lorem sed risus. Consequat interdum varius sit amet mattis vulputate enim nulla aliquet. Enim sed faucibus turpis in eu mi bibendum. Eu consequat ac felis donec et odio pellentesque. Cursus eget nunc scelerisque viverra mauris in aliquam sem. Accumsan in nisl nisi scelerisque eu ultrices vitae auctor eu. Quis commodo odio aenean sed adipiscing. Et ultrices neque ornare aenean euismod elementum nisi. Turpis in eu mi bibendum neque egestas congue. Sed arcu non odio euismod. Risus quis varius quam quisque.<br><br>
+
+Consequat interdum varius sit amet mattis vulputate enim nulla. Tristique et egestas quis ipsum suspendisse. Amet massa vitae tortor condimentum lacinia quis vel eros donec. Varius vel pharetra vel turpis nunc eget. Convallis posuere morbi leo urna molestie at elementum eu facilisis. Congue eu consequat ac felis donec et odio pellentesque. Nunc sed velit dignissim sodales ut eu sem integer. Mattis enim ut tellus elementum sagittis. Molestie ac feugiat sed lectus. Cursus vitae congue mauris rhoncus aenean vel elit. Mauris augue neque gravida in. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Et tortor at risus viverra adipiscing at in tellus integer.</p>
+		</div>
 	</main>
 </body>
 </html>
