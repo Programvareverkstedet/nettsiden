e7f11aee5477042c614302ab939e053ec2264321
Jørn Åne <yorinad@pvv.ntnu.no>
2018-02-13 16:46:01 +0100

    Improve login/logout buttons

--- inc/navbar.php
+++ inc/navbar.php
@@ -29,20 +29,31 @@ function loginBar($sp = 'default-sp') {
 	require_once(__DIR__ . '/../vendor/simplesamlphp/simplesamlphp/lib/_autoload.php');
 	$as = new SimpleSAML_Auth_Simple($sp);
 
-	$svg = '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 64 64">
+	$svgWhite = '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 64 64">
 	<circle cx="32" cy="27" r="14" stroke-width="0" fill="white" />
 	<ellipse cx="32" cy="66" rx="24" ry="28" stroke-width="0" fill="white" />
+</svg>
+';
+	$svgBlue = '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 64 64">
+	<circle cx="32" cy="27" r="14" stroke-width="0" fill="#024" />
+	<ellipse cx="32" cy="66" rx="24" ry="28" stroke-width="0" fill="#024" />
 </svg>
 ';
 
 	$attr = $as->getAttributes();
 	if($attr) {
 		$uname = $attr['uid'][0];
-		$result .= "\t<p id=\"login\">logget inn som: " . htmlspecialchars($uname) . "</p>\n";
+		$result .= "\t<a id=\"login\" href=\"#usermenu\" aria-hidden=\"true\">${svgWhite}" . htmlspecialchars($uname) . "</a>\n";
 	} else {
-		$result .= "\t<a id=\"login\" href=\"" . htmlspecialchars($as->getLoginURL()) . "\">${svg}Logg inn</a>\n";
+		$result .= "\t<a id=\"login\" href=\"" . htmlspecialchars($as->getLoginURL()) . "\">${svgWhite}Logg inn</a>\n";
 	}
-	$result .= "\t<a href=\"#menu\" id=\"menu_toggle\"><big>&#9776;&nbsp;</big>MENU</a>\n";
+
+	$result .= "\n\t<ul id=\"usermenu\">\n";
+	$result .= "\n\t\t<li><a id=\"login\" href=\"#\">${svgBlue}" . htmlspecialchars($uname) . "</a></li>\n";
+	$result .= "\n\t\t<li><a href=\"" . htmlspecialchars($as->getLogoutURL()) . "\">Logg ut</a></li>\n";
+	$result .= "\n\t</ul>\n";
+
+	$result .= "\n\t<a href=\"#menu\" id=\"menu_toggle\" aria-hidden=\"true\"><big>&#9776;&nbsp;</big>MENU</a>\n";
 
 	return $result;
 }
