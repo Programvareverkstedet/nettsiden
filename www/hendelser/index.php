5412414ce1909934ce244d660456d48abe5064a5
Markus <markus@halvorsenfamilien.com>
2018-02-24 17:56:42 +0100

    Assimilate hendelser into the new standard

--- www/hendelser/index.php
+++ www/hendelser/index.php
@@ -5,51 +5,51 @@ $translation = ['I dag', 'I morgen', 'Denne uka', 'Neste uke', 'Denne måneden',
 ?>
 <!DOCTYPE html>
 <html lang="no">
-<title>Hendelsesverkstedet</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
-<link rel="stylesheet" href="../css/normalize.css">
-<link rel="stylesheet" href="../css/style.css">
-<link rel="stylesheet" href="../css/events.css">
+<head>
+	<title>Hendelsesverkstedet</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+	<link rel="stylesheet" href="../css/normalize.css">
+	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="../css/events.css">
+</head>
 
-<header>Hendelses&shy;verk&shy;stedet</header>
+<body>
+	<main>
+		<h2>Hendelser</h2>
 
-<main>
-<article>
+		<?php
+		$description_paragraphs = 1; //description length
+		foreach($agenda->getNextDays() as $period => $events) {
+			if ($events) { ?>
+				<h2><?= $translation[$period] ?></h2>
+				<ul class="events">
+				<?php foreach($events as $event) {?>
+				<li style="border-color: <?= $event->getColor() ?>">
+					<h4><strong>
+						<?php if ($event->getURL()) { ?>
+							<a href="<?= $event->getURL() ?>"><?= $event->getName() ?></a>
+						<?php } else { ?>
+							<?= $event->getName() ?>
+						<?php } ?>
+					</strong></h4>
 
-<?php
-$description_paragraphs = 1; //description length
-foreach($agenda->getNextDays() as $period => $events) {
-	if ($events) { ?>
-		<h2><?= $translation[$period] ?></h2>
-		<ul class="events">
-		<?php foreach($events as $event) {?>
-		<li style="border-color: <?= $event->getColor() ?>">
-			<h4><strong>
-				<?php if ($event->getURL()) { ?>
-					<a href="<?= $event->getURL() ?>"><?= $event->getName() ?></a>
-				<?php } else { ?>
-					<?= $event->getName() ?>
+					<?php $description = $event->getDescription(); ?>
+					<?php if ($description_paragraphs) array_splice($description, $description_paragraphs); ?>
+					<p><?= implode($description, "<br>") ?></p>
+
+					<ul class="subtext">
+						<li>Tid: <strong><?= Agenda::getFormattedDate($event->getStart()) ?></strong></li>
+						<li>Sted: <strong><?= $event->getLocation() ?></strong></li>
+						<li>Arrangør: <strong><?= $event->getOrganiser() ?></strong></li>
+					</ul>
+				</li>
 				<?php } ?>
-			</strong></h4>
-
-			<?php $description = $event->getDescription(); ?>
-			<?php if ($description_paragraphs) array_splice($description, $description_paragraphs); ?>
-			<p><?= implode($description, "<br>") ?></p>
-
-			<ul class="subtext">
-				<li>Tid: <strong><?= Agenda::getFormattedDate($event->getStart()) ?></strong>
-				<li>Sted: <strong><?= $event->getLocation() ?></strong>
-				<li>Arrangør: <strong><?= $event->getOrganiser() ?></strong>
-			</ul>
-
+				</ul>
+			<?php } ?>
 		<?php } ?>
-		</ul>
-	<?php } ?>
-<?php } ?>
-</article>
-
-</main>
+	</main>
+</body>
 
 <nav>
 	<?= navbar(1, 'hendelser'); ?>
