6c7241a2ace5b72189cf19f3db61f527ba1a66f5
Peder Bergebakken Sundt <pbsds@hotmail.com>
2018-08-06 23:29:11 +0200

    Add /mail
    This page will replace the current webmail subdomain

---
+++ www/mail/index.php
@@ -0,0 +1,31 @@
+<?php 
+require_once dirname(dirname(__DIR__)) . implode(DIRECTORY_SEPARATOR, ['', 'inc', 'include.php']);
+?>
+<!DOCTYPE html>
+<html lang=no>
+<style>
+p {hyphens: auto;}
+</style>
+<title>Programvareverkstedet</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+<link rel="stylesheet" href="../css/normalize.css">
+<link rel="stylesheet" href="../css/style.css">
+<link rel="stylesheet" href="../css/mail.css">
+
+<header>Programvareverkstedet</header>
+
+<main>
+
+<ul id="webmail">
+	<li id="squirrelmail"><div><a href="https://webmail.pvv.ntnu.no/squirrelmail/">squirrelmail</a>
+	<li id="roundcube"><div><a href="https://webmail.pvv.ntnu.no/roundcube/">roundcube</a>
+	<li id="rainloop"><div><a href="https://webmail.pvv.ntnu.no/rainloop/">rainloop</a>
+</ul>
+
+</main>
+
+<nav>
+	<?= navbar(1); ?>
+	<?= loginbar($sp, $pdo); ?>
+</nav>
