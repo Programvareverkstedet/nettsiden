6601ec041e95ee36cdac144952d3ba18dddc5bb2
Peder Bergebakken Sundt <pbsds@hotmail.com>
2018-08-09 00:41:54 +0200

    Make list of projects look nice. Added Drift and Styret on the top

--- www/prosjekt/index.php
+++ www/prosjekt/index.php
@@ -5,7 +5,6 @@ $translation = ['i dag', 'i morgen', 'denne uka', 'neste uke', 'denne måneden',
 $projectManager = new \pvv\side\ProjectManager($pdo);
 $projects = $projectManager->getAll();
 ?>
-
 <!DOCTYPE html>
 <html lang="no">
 <head>
@@ -26,21 +25,60 @@ $projects = $projectManager->getAll();
 	</nav>
 
 	<main>
-		<h2>Prosjekter</h2>
+		<h2>Projekter</h2>
+		<p>
+			PVV er et kult miljø der du kan bli med på mye rart.
+			På denne siden har vi en liten oversikt over forskjellige prosjekter og organer i PVV.
+		</p>
+		<br>
+		<br>
+		<h2>Faste poster</h2>
+		<div class="projects-container">
+			<a class="nostyle" href="../styret/"><div class="project-card" style="border-color:#b93;">
+				<div class="card-content">
+					<h4 class="project-title">Styret</h4>
+					<p>
+						Styret har ansvaret for den daglige driften av PVV, og har myndighet som gitt i PVVs lover.
+						Lederen for PVV velges om høsten og sitter i et år. Resten av styret velges for et halvår om
+						gangen, selv om praksis er at bare mindre justeringer gjøres i vårsemesteret. 
+					</p>
+					<p class="project-organizer">Organisert av Styreleder</p>
+				</div>
+			</div></a>
+			<a class="nostyle" href="../drift/"><div class="project-card" style="border-color:#184;">
+				<div class="card-content">
+					<h4 class="project-title">Drift</h4>
+					<p>
+						Drift har ansvaret for å drive maskinene på PVV. 
+						Driftsgruppen har ingen strenge krav til aktivitet eller erfaring for å bli medlem,
+						så selv om du er ny i virket går det fint an å bare observere og absorbere i begynnelsen.
+						Vi vil gjøre vårt beste for å gi god hjelp og service til våre brukere.
+					</p>
+					<p class="project-organizer">Organisert av Driftskordinator</p>
+				</div>
+			</div></a>
+			<?php /*Her kan vi legge til PR og TriKom hvis det er ønskelig*/ ?>
+		</div>
+		
+		<h2>Medlems-prosjekter</h2>
 		<?php
 			if(count($projects) == 0){
 		?>
 			<p>PVV har for øyeblikket ingen aktive prosjekter. Tenker du at noe bør gjøres? Har du en kul ide for noe PVV kan samarbeide om? Sett opp et prosjekt!</p>
 			<br>
-			<a class="btn" href="edit.php?new=1">Lag prosjekt</a>
-			<a class="btn" href="mine.php">Mine prosjekter</a>
+			<center>
+				<a class="btn" href="edit.php?new=1">Lag prosjekt</a>
+				<a class="btn" href="mine.php">Mine prosjekter</a>
+			</center>
+			<br>
 		<?php
 			}else{
 		?>
-			<p>Lyst til å gjøre noe kult? Her er et utvalg av de prosjektene som PVVere har laget. Mangler det noe, eller brenner du for noe annet? Sett opp et eget prosjekt!</p>
-			<a class="btn" href="edit.php?new=1">Lag prosjekt</a>
-			<a class="btn" href="mine.php">Mine prosjekter</a>
-			<hr class="projects-divider">
+			<p>
+				Lyst til å gjøre noe kult? Her er et utvalg av de prosjektene som PVVere har holder på med. Mangler det noe, eller brenner du for noe annet?<br>
+				Sett opp et eget prosjekt da vel!
+			</p>
+			<div class="projects-container">
 
 			<?php
 				$randProjects = array_rand($projects, min(6, count($projects)));
@@ -55,11 +93,16 @@ $projects = $projectManager->getAll();
 			<div class="project-card">
 				<div class="card-content">
 					<h4 class="project-title"><?= $project->getName(); ?></h4>
-					<p><?= $project->getDescription()[0]; ?></p>
+					<p><?= implode('<br>',array_slice($project->getDescription(), 0, 2)); ?></p>
 				</div>
-				<p class="project-organizer">Organisert av<br><?= $owner['name']; ?></p>
+				<p class="project-organizer">Organisert av <?= $owner['name']; ?></p>
 			</div>
 			<?php } ?>
+			</div>
+			<center>
+				<a class="btn" href="edit.php?new=1">Lag prosjekt</a>
+				<a class="btn" href="mine.php">Mine prosjekter</a>
+			</center>
 		<?php
 			}
 		?>
