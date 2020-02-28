d1734e8762312179f22d27e34e94bd34c40aae21
Peder Bergebakken Sundt <pbsds@hotmail.com>
2020-02-28 16:26:57 +0100

    Broen er oppe

--- www/kontakt/index.php
+++ www/kontakt/index.php
@@ -33,7 +33,9 @@ require_once dirname(dirname(__DIR__)) . implode(DIRECTORY_SEPARATOR, ['', 'inc'
 		<p> Vi har en IRC-kanal på <a href="http://webchat.ircnet.net/">IRCnet</a> kalt #pvv.
 
 		<p> Vi har også en Discord-kanal. Denne kanalen er satt opp slik at man i Discord-kanalen ser hva som skrives i IRC-kanalen, og vice versa. For å bli med i Discord-kanalen, <a href="https://discord.gg/8VTBr6Q">klikk her</a>.
+		<!--
 			<i>(Denne broen for øyeblikket ute av drift)</i>
+		-->
 
 		<p> Det er også mulig å ta i bruk analog kontakt ved å møte opp <a href="https://use.mazemap.com/?v=1&left=10.4032&right=10.4044&top=63.4178&bottom=63.4172&campusid=1&zlevel=2&sharepoitype=point&sharepoi=10.40355%2C63.41755%2C2&utm_medium=longurl">på stripa</a>.
 
