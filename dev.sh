523b30d6f5e3f97848325bf3933ef0c84383097c
Peder Bergebakken Sundt <pbsds@hotmail.com>
2021-03-16 20:00:47 +0100

    Fix dev.sh

--- dev.sh
+++ dev.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-which sqlite3 > /dev/null 2>&1 && test \! -e pvv.sqlite && sqlite3 pvv.sqlite < dist/pvv.sql
-test \! -e sql_config.php && cp -v dist/sql_config_example.php sql_config.php
+which sqlite3 > /dev/null 2>&1 || (echo ERROR: sqlite not found; false) || exit 1
+test ! -e pvv.sqlite && sqlite3 pvv.sqlite < dist/pvv.sql
+test ! -e sql_config.php && cp -v dist/sql_config_example.php sql_config.php
 
-test \! -e dataporten_config.php && cp -v dist/dataporten_config.php dataporten_config.php
+test ! -e dataporten_config.php && cp -v dist/dataporten_config.php dataporten_config.php
 
 test -e composer.phar || curl -O https://getcomposer.org/composer.phar
 
-if test \! -d vendor; then
-	php composer.phar install
+if test ! -d vendor; then
+	php composer.phar install || exit $?
 	cp -v dist/authsources_example.php vendor/simplesamlphp/simplesamlphp/config/authsources.php
 	cp -v dist/saml20-idp-remote.php vendor/simplesamlphp/simplesamlphp/metadata/saml20-idp-remote.php
 	# cp -v vendor/simplesamlphp/simplesamlphp/config-templates/config.php vendor/simplesamlphp/simplesamlphp/config/config.php
