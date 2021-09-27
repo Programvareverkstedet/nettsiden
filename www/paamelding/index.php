0d87d37c726f334f144fd5867c95b9bc9f6a4bf2
Felix Albrigtsen <felixalbrigtsen@gmail.com>
2021-09-27 23:25:04 +0200

    Oppdatert romnummer og kontaktinformasjon

--- www/paamelding/index.php
+++ www/paamelding/index.php
@@ -24,25 +24,6 @@ if (isset($_GET['code'])) {
 	die();
 }
 
-if (isset($_SESSION['userdata'])) { // if logged in with feide
-	$mailHeaders  = "MIME-Version: 1.0" . "\r\n";
-	$mailHeaders .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-	$mailHeaders .= 'From: <spikkjeposche@pvv.ntnu.no>' . "\r\n";
-	$mailHeaders .= 'Cc: <' . htmlspecialchars($_SESSION['userdata']['user']['email']) .'>' . "\r\n";
-	$mailParams   = "-fspikkjeposche@pvv.ntnu.no";
-	$mailTo       = "nybruker@pvv.ntnu.no";
-	$mailSubject  = "Nytt medlem for PVV";
-	$mailBody
-		= "Hei, jeg vil bli medlem på PVV.\n"
-		. "Navn: " . htmlspecialchars($_SESSION['userdata']['user']['name']) . "\n"
-		. "Brukernavn: " . htmlspecialchars($_SESSION['userdata']['user']['userid_sec'][0]) . "\n"
-		. "Epost: " . htmlspecialchars($_SESSION['userdata']['user']['email']) . "\n"
-		. "Jeg skal betale medlemsavgiften, og kommer innom PVVs lokaler for å aktivere kontoen min";
-	if (isset($_GET['send_mail'])) { // if logged in with feide
-		mail($mailTo, $mailSubject, $mailBody, $mailHeaders, $mailParams);
-	}
-}
-
 ?>
 <!DOCTYPE html>
 <html lang="no">
@@ -88,37 +69,18 @@ if (isset($_SESSION['userdata'])) { // if logged in with feide
 		<p>
 			PVV har for øyeblikket et manuelt system for å legge til nye brukere.
 			Se lenkene over for mer informasjon.
-			Vi foretrekker at du kommer inn på besøk på <a href="https://use.mazemap.com/?v=1&left=10.4032&right=10.4044&top=63.4178&bottom=63.4172&campusid=1&zlevel=2&sharepoitype=point&sharepoi=10.40355%2C63.41755%2C2&utm_medium=longurl">våre lokaler på stripa</a>
-			for å sette sette opp din PVV bruker. Hvis du vil, kan du også sende oss
-			en melding fra denne nettsiden med ditt navn, epost og NTNU brukernavn.
+			Vi foretrekker at du kommer inn på besøk på <a href="https://link.mazemap.com/aKDz8eu8">våre lokaler i Oppredning/Gruvedrift, rom 247</a>
+			for å sette sette opp din PVV bruker. Hvis du ikke har mulighet til det, kan du <a href="../pvv/Kontaktinformasjon">finne oss her</a> og sende en epost.
 			For å aktivere din brukerkonto på PVV, må du alikevell møte opp på
 			lokalene våre slik at vi kan få satt ditt passord.
 		</p>
+		<iframe width="600" height="420" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://use.mazemap.com/embed.html#v=1&zlevel=2&center=10.406194,63.417143&zoom=18&campusid=1&sharepoitype=poi&sharepoi=38159&utm_medium=iframe" style="border: 1px solid grey" allow="geolocation"></iframe><br/>
 	
 		<?php if($attrs) { //logged in with pvv account?>
 			<p>
 				Du er nå logget in som <i><?= htmlspecialchars($attrs['uid'][0]) ?></i>,
 				og trenger klart ikke sende melding om å få ny PVV bruker.
 			</p>
-		<?php } elseif (isset($_SESSION['userdata'])) { //logged in with feide ?>
-			<?php if (! isset($_GET['send_mail'])) { ?>
-				<h3>Meldingen som du nå sender:</h3>
-				<code>
-					Til: <?=$mailTo?><br>
-					Fra: nettsiden<br>
-					Tittel: <?=$mailSubject?><br>
-					<br/>
-					<?= nl2br($mailBody) ?>
-				</code><br>
-				<br>
-				<a class="btn" href=".?send_mail#sent">Send!</a>
-			<?php } else { // not logged in?>
-				<p id="sent">
-					Meldingen har blitt sendt!
-				</p>
-			<?php }?>
-		<?php } else { // not logged in?>
-			<a class="btn" href=".?login">Hent navn og epost fra Feide</a>
-		<?php }?>
+		<?php } ?>
 	</main>
 </body>
