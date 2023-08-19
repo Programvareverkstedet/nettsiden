0566ed2aeed29a61680ed1fe8d97cb8412f02661
Felix Albrigtsen <felix@albrigtsen.it>
2023-08-20 01:30:49 +0200

    Minor fixes to contact page

--- www/kontakt/index.php
+++ www/kontakt/index.php
@@ -22,26 +22,32 @@ require_once dirname(dirname(__DIR__)) . implode(DIRECTORY_SEPARATOR, ['', 'inc'
 	</nav>
 
 	<main>
-		<h2>Kontaktinformasjon<h2>
-		<img style="width: 8em" src="kontakt.jpg"></img>
+		<h2>
+			Kontaktinformasjon
+			<a href="https://en.wikipedia.org/wiki/IP_over_Avian_Carriers">
+				<img style="width: 8em" src="kontakt.jpg">
+			</a>
+		</h2>
+
 		<p>
-			Vi kan kontaktes på følgende epost adresser:
+			Vi kan kontaktes på følgende e-postadresser:
 			<ul>
 				<li><a href="mailto:pvv@pvv.ntnu.no">pvv@pvv.ntnu.no</a> for hendvendelser til styret</li>
-				<li><a href="mailto:drift@pvv.ntnu.no">styret@pvv.ntnu.no</a> for hendvendelser anngående våre datasystemer</li>
+				<li><a href="mailto:drift@pvv.ntnu.no">drift@pvv.ntnu.no</a> for hendvendelser anngående våre datasystemer</li>
 			</ul>
 		</p>
-		<p> Det er også mulig å ta kontakt med oss ved å møte opp <a href="https://bit.ly/31P6yr5">på våre lokaler i oppredning/gruvedrift</a>.
+
+		<p>Det er også mulig å ta kontakt med oss ved å møte opp <a href="https://link.mazemap.com/JqgWGSnh">på våre lokaler i oppredning/gruvedrift</a>.</p>
 
 		<h2>Kommunikasjonskanaler</h2>
 
-		<p> Vi har en <a href="http://list.pvv.org/mailman/listinfo/aktive">epostliste for aktive medlemmer</a>. All offisiell informasjon blir sendt på denne listen, og alle arrengementer blir også annonsert her.
+		<p>Vi har en <a href="http://list.pvv.org/mailman/listinfo/aktive">e-postliste for aktive medlemmer</a>. All offisiell informasjon blir sendt på denne listen, og alle arrengementer blir også annonsert her.</p>
 
-		<p> Vi har en <a href="https://matrix.to/#/#pvv:pvv.ntnu.no">Matrix server</a> for chat, memes, og all annen intern kommunikasjon. Den er bridget med IRC-kanalen og Discord serveren vår. Hvis du er medlem kan du bruke vår <a href="https://chat.pvv.ntnu.no">self-hosted web client</a>.
+		<p>Vi har en <a target="_blank" href="https://matrix.to/#/#pvv:pvv.ntnu.no">Matrix-server</a> for chat, memes, og all annen kommunikasjon. Den er bridget med IRC-kanalen og Discord-guilden vår. Hvis du er medlem kan du bruke vår <a href="https://chat.pvv.ntnu.no">self-hosted web client</a>.</p>
 		
-		<p> Vi har en IRC-kanal på <a href="http://webchat.ircnet.net/">IRCnet</a> kalt #pvv.
+		<p>Vi har en IRC-kanal på <a href="http://webchat.ircnet.net/">IRCnet</a> kalt #pvv.</p>
 
-		<p> Vi har en <a href="https://discord.gg/8VTBr6Q">Discord server</a> for de som foretrekker Discord over Matrix.
+		<p>Vi har en <a target="_blank" href="https://discord.gg/8VTBr6Q">Discord-guild</a> for de som foretrekker Discord over Matrix. </p>
 
 	</main>
 </body>
