0542a10d03fe9689b6a038e2579e7798f4d48a28
Felix Albrigtsen <felix@albrigtsen.it>
2024-05-14 19:53:35 +0200

    spaceapi: Moves calendar into feeds
    Co-authored-by: h7x4 <h7x4@nani.wtf>

--- www/spaceapi.php
+++ www/spaceapi.php
@@ -12,9 +12,12 @@ $doorEntry = (object)($door->getCurrent());
 {
   "api_compatibility": ["14"],
   "space": "Programvareverkstedet",
-  "logo": "https://git.pvv.ntnu.no/Drift/pvv-nixos-config/raw/branch/main/assets/logo_blue_regular.png",
+  "logo": "https://git.pvv.ntnu.no/assets/img/logo.png",
   "url": "https://www.pvv.ntnu.no/",
   "location": {
+    "ext_campus": "NTNU Gløshaugen",
+    "ext_room_name": "Oppredning/Gruvedrift, Floor 2, Room 247",
+    "ext_mazemap": "https://link.mazemap.com/2n2HWa7H",
     "address": "Sem Sælands vei 1, 7034 Trondheim, Norway",
     "timezone": "Europe/Oslo",
     "lon": 10.242,
@@ -30,12 +33,17 @@ $doorEntry = (object)($door->getCurrent());
   "issue_report_channels": ["email"],
   "state": {
     "open": <?php echo($doorEntry->open ? "true" : "false"); ?>,
-    "lastchange": <?php echo($doorEntry->time ? $doorEntry->time : 0); ?>
+    "lastchange": <?php echo($doorEntry->time ? $doorEntry->time : 0); ?>,
+    "message": "<?php echo($doorEntry->open ? "open for public, members are present" : "closed"); ?>"
   },
   "feeds": {
     "wiki": {
       "type": "atom",
       "url": "https://www.pvv.ntnu.no/w/api.php?hidebots=1&urlversion=1&action=feedrecentchanges&feedformat=atom"
+    },
+    "calendar": {
+      "type": "html",
+      "url": "https://www.pvv.ntnu.no/hendelser/"
     }
   },
   "projects": [
@@ -44,11 +52,6 @@ $doorEntry = (object)($door->getCurrent());
     "https://www.pvv.ntnu.no/prosjekt/"
   ],
   "links": [
-    {
-      "name": "Events",
-      "url": "https://www.pvv.ntnu.no/hendelser/",
-      "description": "Upcoming events at Programvareverkstedet"
-    },
     {
       "name": "YouTube",
       "url": "https://www.youtube.com/@pvvntnu5640"
