4d4ec78ea3796258da4ecb4caae56b953f20f6e0
h7x4 <h7x4@nani.wtf>
2025-03-18 21:17:36 +0100

    pvv_sqlite.sql: format

--- dist/pvv_sqlite.sql
+++ dist/pvv_sqlite.sql
@@ -1,50 +1,54 @@
 CREATE TABLE "events" (
-"id" INTEGER PRIMARY KEY AUTOINCREMENT,
-"name" TEXT,
-"start" TEXT,
-"stop" TEXT,
-"organiser" TEXT,
-"location" TEXT,
-"description" TEXT
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "name" TEXT,
+  "start" TEXT,
+  "stop" TEXT,
+  "organiser" TEXT,
+  "location" TEXT,
+  "description" TEXT
 );
 
 CREATE TABLE "projects" (
-"id" INTEGER PRIMARY KEY AUTOINCREMENT,
-"name" TEXT,
-"description" TEXT,
-"active" BOOLEAN
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "name" TEXT,
+  "description" TEXT,
+  "active" BOOLEAN
 );
 
 CREATE TABLE "projectmembers" (
-"projectid" INTEGER,
-"name" TEXT,
-"uname" TEXT,
-"mail" TEXT,
-"role" TEXT,
-"lead" BOOLEAN DEFAULT 0,
-"owner" BOOLEAN DEFAULT 0
+  "projectid" INTEGER,
+  "name" TEXT,
+  "uname" TEXT,
+  "mail" TEXT,
+  "role" TEXT,
+  "lead" BOOLEAN DEFAULT 0,
+  "owner" BOOLEAN DEFAULT 0
 );
 
-CREATE TABLE "users" (
-"uname" TEXT,
-"groups" INT DEFAULT 0
-);
+CREATE TABLE "users" ("uname" TEXT, "groups" INT DEFAULT 0);
 
 CREATE TABLE "motd" (
-"id" INTEGER PRIMARY KEY AUTOINCREMENT,
-"title" TEXT,
-"content" TEXT
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "title" TEXT,
+  "content" TEXT
 );
-INSERT INTO motd (title, content)
-VALUES ('MOTD ./dev.sh', 'du kan endre motd i admin panelet');
 
-CREATE TABLE "door" (
-"time" INTEGER PRIMARY KEY,
-"open" BOOLEAN
-);
-INSERT INTO door (time, open)
-VALUES (0, FALSE);
+INSERT INTO
+  motd (title, content)
+VALUES
+  (
+    'MOTD ./dev.sh',
+    'du kan endre motd i admin panelet'
+  );
 
+CREATE TABLE "door" ("time" INTEGER PRIMARY KEY, "open" BOOLEAN);
 
-INSERT INTO users (uname, groups)
-VALUES ('min_test_bruker', 1);
+INSERT INTO
+  door (time, open)
+VALUES
+  (0, FALSE);
+
+INSERT INTO
+  users (uname, groups)
+VALUES
+  ('min_test_bruker', 1);
