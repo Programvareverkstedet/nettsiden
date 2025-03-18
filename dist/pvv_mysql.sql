02cfe9feaafb7159ae1d4afe0ba4286ef76dbff9
h7x4 <h7x4@nani.wtf>
2025-03-18 21:18:24 +0100

    pvv_mysql.sql: format

--- dist/pvv_mysql.sql
+++ dist/pvv_mysql.sql
@@ -1,47 +1,45 @@
 CREATE TABLE events (
-`id` INTEGER PRIMARY KEY AUTO_INCREMENT,
-`name` TEXT,
-`start` TEXT,
-`stop` TEXT,
-`organiser` TEXT,
-`location` TEXT,
-`description` TEXT
+  `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+  `name` TEXT,
+  `start` TEXT,
+  `stop` TEXT,
+  `organiser` TEXT,
+  `location` TEXT,
+  `description` TEXT
 );
 
-CREATE TABLE projects ( 
-`id` INTEGER PRIMARY KEY AUTO_INCREMENT,
-`name` TEXT,
-`description` TEXT,
-`active` BOOLEAN
+CREATE TABLE projects (
+  `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+  `name` TEXT,
+  `description` TEXT,
+  `active` BOOLEAN
 );
 
 CREATE TABLE projectmembers (
-`projectid` INTEGER,
-`name` TEXT,
-`uname` TEXT,
-`mail` TEXT,
-`role` TEXT,
-`lead` BOOLEAN DEFAULT 0,
-`owner` BOOLEAN DEFAULT 0
+  `projectid` INTEGER,
+  `name` TEXT,
+  `uname` TEXT,
+  `mail` TEXT,
+  `role` TEXT,
+  `lead` BOOLEAN DEFAULT 0,
+  `owner` BOOLEAN DEFAULT 0
 );
 
-CREATE TABLE users (
-`uname` TEXT,
-`groups` INT DEFAULT 0
-);
+CREATE TABLE users (`uname` TEXT, `groups` INT DEFAULT 0);
 
 CREATE TABLE motd (
-`id` INTEGER PRIMARY KEY AUTO_INCREMENT,
-`title` TEXT,
-`content` TEXT
+  `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
+  `title` TEXT,
+  `content` TEXT
 );
+
 /*
 INSERT INTO motd (title, content)
 VALUES ("MOTD ./dev.sh", "du kan endre motd i admin panelet");
 */
+CREATE TABLE door (`time` INTEGER PRIMARY KEY, `open` BOOLEAN);
 
-CREATE TABLE door (
-`time` INTEGER PRIMARY KEY,
-`open` BOOLEAN
-);
-INSERT INTO door(time, open) VALUES (0, FALSE);
+INSERT INTO
+  door (time, open)
+VALUES
+  (0, FALSE);
