158e816ed0f7dc0d6137579034a0e5d7b26e0c69
h7x4 <h7x4@nani.wtf>
2025-12-17 22:02:23 +0900

    Add the dev auth users to the database

---
+++ dist/sql/test_data_sqlite.sql
@@ -0,0 +1,7 @@
+
+-- See users in ../authsources.php
+INSERT INTO
+  users (uname, groups)
+VALUES
+  ('admin', 1 | 2 | 4),
+  ('user', 0);
--- scripts/seed-test-data.sh
+++ scripts/seed-test-data.sh
@@ -2,7 +2,9 @@
 
 set -euo pipefail
 
-REQUIRED_COMMANDS=(git)
+REQUIRED_COMMANDS=(
+  sqlite3
+)
 MISSING_COMMANDS=false
 for cmd in "${REQUIRED_COMMANDS[@]}"; do
   if ! command -v "$cmd" &> /dev/null; then
@@ -16,6 +18,9 @@ fi
 
 declare -r PROJECT_ROOT="$(git rev-parse --show-toplevel)"
 
-"$PROJECT_ROOT/scripts/clean.sh"
-"$PROJECT_ROOT/scripts/setup.sh"
-"$PROJECT_ROOT/scripts/seed-test-data.sh"
+if [ ! -f "$PROJECT_ROOT/pvv.sqlite" ] ; then
+  echo "Database file $PROJECT_ROOT/pvv.sqlite does not exist. Please run setup.sh first." >&2
+  exit 1
+fi
+
+sqlite3 "$PROJECT_ROOT/pvv.sqlite" < "$PROJECT_ROOT/dist/sql/test_data_sqlite.sql"
