b9992c7c577711640c17341edf5b3f9964272915
h7x4 <h7x4@nani.wtf>
2026-01-13 11:22:51 +0900

    dist/sql: format test data script

--- dist/sql/test_data_sqlite.sql
+++ dist/sql/test_data_sqlite.sql
@@ -1,13 +1,13 @@
 
 -- See users in ../authsources.php
 INSERT INTO
-  users (uname, groups)
+  "users"("uname", "groups")
 VALUES
   ('admin', 1 | 2 | 4),
   ('user', 0);
 
 INSERT INTO
-  motd (title, content)
+  "motd"("title", "content")
 VALUES
   (
     'MOTD ./dev.sh',
