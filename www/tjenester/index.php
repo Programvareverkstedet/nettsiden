cc8adf3f84483cb1dceba720d031bd02c235cccf
h7x4 <h7x4@nani.wtf>
2026-01-13 12:17:56 +0900

    www/tjenester/index: pull content out of html

--- www/tjenester/index.php
+++ www/tjenester/index.php
@@ -3,6 +3,123 @@
 namespace pvv\side;
 
 require_once \dirname(__DIR__, 2) . implode(\DIRECTORY_SEPARATOR, ['', 'inc', 'include.php']);
+
+$services = [
+    "Versjonskontroll og utvikling" => [
+        [
+            "name" => "Gitea",
+            "description" => "Vår interne git-tjener, åpen for alle medlemmer",
+            "link" => "https://git.pvv.ntnu.no",
+            "link_text" => "Gå til git.pvv.ntnu.no",
+            "image" => "img/gitea.png",
+        ],
+        [
+            "name" => "GitHub",
+            "description" => "Våre offentlige kodebrønner, åpent for verden!",
+            "link" => "https://github.com/Programvareverkstedet/",
+            "link_text" => "Gå til GitHub",
+            "image" => "img/github.png",
+        ],
+    ],
+    "Kommunikasjon" => [
+        [
+            "name" => "Matrix via Element",
+            "description" => implode(
+                " ",
+                [
+                  "Åpen kommunikasjonsprotokoll som støtter ende-til-ende-kryptering og utallige kule funksjoner.",
+                  "Vårt space er bridget sammen med Discord, så du får alle de samme meldingene.",
+                  "#pvv:pvv.ntnu.no",
+                ],
+            ),
+            "link" => "https://chat.pvv.ntnu.no",
+            "link_text" => "Gå til chat.pvv.ntnu.no",
+            "image" => "img/element.png",
+        ],
+        [
+            "name" => "Discord",
+            "description" => "Vår hovedkanal, her finner du alt fra ofisielle announcements til memes og driftsdiskusjoner.",
+            "link" => "https://discord.gg/WpaHGV8K",
+            "link_text" => "Gå til Discord",
+            "image" => "img/discord.png",
+        ],
+        [
+            "name" => "Epost",
+            "description" => "Som PVV-medlem får du din egen @pvv.ntnu.no-adresse, som kan brukes med alle vanlige epostprotokoller.",
+            "link" => "https://www.pvv.ntnu.no/mail/",
+            "link_text" => "Gå til Webmail",
+            "image" => "img/email.png",
+        ],
+        [
+            "name" => "IRC",
+            "description" => "Hvis Discord er for proprietært og Matrix er for hypermoderne er kanskje IRC for deg. Vi har en kanal på IRCNet, #pvv.",
+            "link" => "irc://irc.pvv.ntnu.no/pvv",
+            "link_text" => "Koble til med IRC",
+            "image" => "img/irc.png",
+        ],
+    ],
+    "Hosting" => [
+        [
+            "name" => "Brukernettsider",
+            "description" => "Alle brukere får automatisk en egen side for html og php. Denne er offentlig på pvv.ntnu.no/~brukernavn.",
+            "link" => "https://wiki.pvv.ntnu.no/wiki/Tjenester/Hjemmesider",
+            "link_text" => "Gå til dokumentasjon på wiki",
+            "image" => "img/php.png",
+        ],
+        [
+            "name" => "PVV-siden",
+            "description" => "Du befinner deg nå på PVV sin offisielle hjemmeside. Den er skrevet i PHP og kjører på en egen server.",
+            "link" => "https://git.pvv.ntnu.no/Projects/nettsiden",
+            "link_text" => "Se koden på gitea",
+            "image" => "../pvv-logo.png",
+        ],
+        [
+            "name" => "Proxmox @blossom",
+            "description" => "Blossom er den sterkeste av våre VM-tjenere, her kan du kjøre enten fulle VM-er eller konteinere. Bare Drift har tilgang på disse tjenerne.",
+            "link" => "https://blossom.pvv.ntnu.no:8006",
+            "link_text" => "Gå til blossom.pvv.ntnu.no",
+            "image" => "img/proxmox.png",
+        ],
+        [
+            "name" => "Proxmox @bubbles",
+            "description" => "Bubbles er den svakeste av våre VM-tjenere.",
+            "link" => "https://bubbles.pvv.ntnu.no:8006",
+            "link_text" => "Gå til bubbles.pvv.ntnu.no",
+            "image" => "img/proxmox.png",
+        ],
+        [
+            "name" => "Proxmox @buttercup",
+            "description" => "Buttercup er en av våre VM-tjenere.",
+            "link" => "https://buttercup.pvv.ntnu.no:8006",
+            "link_text" => "Gå til buttercup.pvv.ntnu.no",
+            "image" => "img/proxmox.png",
+        ],
+    ],
+    "Underholdning" => [
+        [
+            "name" => "Minecraft",
+            "description" => "Vi har en egen Minecraft-server for medlemmer, som du kan koble til med IP-adressen minecraft.pvv.ntnu.no. Spør om whitelist på matrix/discord.",
+            "link" => "https://minecraft.pvv.ntnu.no",
+            "link_text" => "Gå til verdenskartet vårt",
+            "image" => "img/minecraft.png",
+        ],
+        [
+            "name" => "MiniFlux RSS reader",
+            "description" => "Trenger du en cross-platform RSS/Atom-leser for å følge med på omverdenen som det er 1990? ",
+            "link" => "https://feeds.pvv.ntnu.no",
+            "link_text" => "Gå til MiniFlux",
+            "image" => "img/rss.png",
+        ],
+        [
+            "name" => "Bildegalleri",
+            "description" => "PVV har et felles bildegalleri, der alle kan legge relevante bilder, som automatisk blir inkludert på nettsiden.",
+            "link" => "https://www.pvv.ntnu.no/galleri/",
+            "link_text" => "Se galleriet",
+            "image" => "img/gallery.png",
+        ],
+    ],
+]
+
 ?>
 <!DOCTYPE html>
 <html lang="no">
@@ -18,186 +135,31 @@ require_once \dirname(__DIR__, 2) . implode(\DIRECTORY_SEPARATOR, ['', 'inc', 'i
 <header>Tjenesteverkstedet</header>
 
 <body>
-        <nav>
-                <?php echo navbar(1, 'tjenester'); ?>
-                <?php echo loginbar($sp, $pdo); ?>
-        </nav>
-        <main>
-
-        <div class="serviceWrapper">
-
-          <div class="categoryContainer">
-            <div class="categoryLabel">Versjonskontroll og utvikling</div>
-            <div class="categoryContent">
-
-              <div class="service">
-                <div class="serviceContent">
-                  <h2 class="serviceTitle">Gitea</h2>
-                  <p class="serviceDescription">Vår interne git-tjener, åpen for alle medlemmer</p>
-                  <div class="serviceLink"><a href="https://git.pvv.ntnu.no" target="_blank">Gå til git.pvv.ntnu.no</a></div>
-                </div>
-                <img class="serviceImage" src="img/gitea.png" alt="Gitea-logo">
-              </div>
-
-	      <div class="service">
-                <div class="serviceContent">
-                  <h2 class="serviceTitle">GitHub</h2>
-                  <p class="serviceDescription">Våre offentlige kodebrønner, åpent for verden!</p>
-                  <div class="serviceLink"><a href="https://github.com/Programvareverkstedet/" target="_blank">Gå til GitHub</a></div>
-                </div>
-                <img class="serviceImage" src="img/github.png" alt="GitHub-logo">
-	      </div>
-
+  <nav>
+    <?php echo navbar(1, 'tjenester'); ?>
+    <?php echo loginbar($sp, $pdo); ?>
+  </nav>
+  <main>
+    <div class="serviceWrapper">
+      <?php foreach ($services as $category => $serviceList): ?>
+      <div class="categoryContainer">
+        <div class="categoryLabel"><?php echo htmlspecialchars($category); ?></div>
+        <div class="categoryContent">
+          <?php foreach ($serviceList as $service): ?>
+          <div class="service">
+            <div class="serviceContent">
+              <h2 class="serviceTitle"><?php echo htmlspecialchars($service['name']); ?></h2>
+              <p class="serviceDescription"><?php echo htmlspecialchars($service['description']); ?></p>
+              <div class="serviceLink"><a href="<?php echo htmlspecialchars($service['link']); ?>" target="_blank"><?php echo htmlspecialchars($service['link_text']); ?></a></div>
             </div>
+            <img class="serviceImage" src="<?php echo htmlspecialchars($service['image']); ?>" alt="<?php echo htmlspecialchars($service['name']); ?>-logo">
           </div>
-
-          <div class="categoryContainer">
-            <div class="categoryLabel">Kommunikasjon</div>
-            <div class="categoryContent">
-
-              <div class="service">
-                <div class="serviceContent">
-                  <h2 class="serviceTitle">Matrix via Element</h2>
-                  <p class="serviceDescription">Åpen kommunikasjonsprotokoll som støtter ende-til-ende-kryptering og utallige kule funksjoner. Vårt space er bridget sammen med Discord, så du får alle de samme meldingene. <b>#pvv:pvv.ntnu.no</b></p>
-                  <div class="serviceLink">
-                    <a href="https://chat.pvv.ntnu.no" target="_blank">Gå til chat.pvv.ntnu.no(medlem)</a>
-                  </div>
-                  <div class="serviceLink">
-                    <a href="https://matrix.to/#/#pvv:pvv.ntnu.no" target="_blank">Gå til #pvv:pvv.ntnu.no(offentlig)</a>
-                  </div>
-                </div>
-                <img class="serviceImage" src="img/element.png" alt="Element-logo">
-              </div>
-
-              <div class="service">
-                <div class="serviceContent">
-                  <h2 class="serviceTitle">Discord</h2>
-                  <p class="serviceDescription">Vår hovedkanal, her finner du alt fra ofisielle announcements til memes og driftsdiskusjoner.</p>
-                  <div class="serviceLink"><a href="https://discord.gg/WpaHGV8K" target="_blank">Gå til Discord</a></div>
-                </div>
-                <img class="serviceImage" src="img/discord.png" alt="Discord-logo">
-              </div>
-
-              <div class="service">
-                <div class="serviceContent">
-                  <h2 class="serviceTitle">Epost</h2>
-                  <p class="serviceDescription">Som PVV-medlem får du din egen @pvv.ntnu.no-adresse, som kan brukes med alle vanlige epostprotokoller.</p>
-                  <div class="serviceLink">
-                    <a href="https://www.pvv.ntnu.no/mail/" target="_blank">Gå til Webmail</a>
-                  </div>
-                  <div class="serviceLink">
-                    <a href="https://wiki.pvv.ntnu.no/wiki/Drift/Mail/IMAP_POP3" target="_blank">IMAP/POP/SMTP-innstillinger</a>
-                  </div>
-                </div>
-                <img class="serviceImage" src="img/email.png" alt="Epost-ikon">
-              </div>
-
-              <div class="service">
-                <div class="serviceContent">
-                  <h2 class="serviceTitle">IRC</h2>
-                  <p class="serviceDescription">Hvis Discord er for proprietært og Matrix er for hypermoderne er kanskje IRC for deg. Vi har en kanal på IRCNet, <b>#pvv</b>.</p>
-                  <div class="serviceLink"><a href="irc://irc.pvv.ntnu.no/pvv" target="_blank">Koble til med IRC</a></div>
-                </div>
-                <img class="serviceImage" src="img/irc.png" alt="IRC-ikon">
-              </div>
-
-            </div>
-          </div>
-
-
-          <div class="categoryContainer">
-            <div class="categoryLabel">Hosting</div>
-            <div class="categoryContent">
-
-              <div class="service">
-                <div class="serviceContent">
-                  <h2 class="serviceTitle">Brukernettsider</h2>
-                  <p class="serviceDescription">Alle brukere får automatisk en egen side for html og php. Denne er offentlig på pvv.ntnu.no/~brukernavn.</p>
-                  <div class="serviceLink"><a href="https://wiki.pvv.ntnu.no/wiki/Tjenester/Hjemmesider" target="_blank">Gå til dokumentasjon på wiki</a></div>
-                </div>
-                <img class="serviceImage" src="img/php.png" alt="En elephpant">
-              </div>
-
-              <div class="service">
-                <div class="serviceContent">
-                  <h2 class="serviceTitle">PVV-siden</h2>
-                  <p class="serviceDescription">Du befinner deg nå på PVV sin offisielle hjemmeside. Den er skrevet i PHP og kjører på en egen server.</p>
-                  <div class="serviceLink"><a href="https://git.pvv.ntnu.no/Projects/nettsiden" target="_blank">Se koden på gitea</a></div>
-                </div>
-                <img class="serviceImage" src="../pvv-logo.png" alt="PVV-logo">
-              </div>
-
-              <div class="service">
-                <div class="serviceContent">
-                  <h2 class="serviceTitle">Proxmox @blossom</h2>
-                  <p class="serviceDescription">Blossom er den sterkeste av våre VM-tjenere, her kan du kjøre enten fulle VM-er eller konteinere. Bare Drift har tilgang på disse tjenerne.</p>
-                  <div class="serviceLink"><a href="https://blossom.pvv.ntnu.no:8006" target="_blank">Gå til blossom.pvv.ntnu.no</a></div>
-                </div>
-                <img class="serviceImage" src="img/proxmox.png" alt="Proxmox-logo">
-              </div>
-
-              <div class="service">
-                <div class="serviceContent">
-                  <h2 class="serviceTitle">Proxmox @bubbles</h2>
-                  <p class="serviceDescription">Bubbles er den svakeste av våre VM-tjenere.</p>
-                  <div class="serviceLink"><a href="https://bubbles.pvv.ntnu.no:8006" target="_blank">Gå til bubbles.pvv.ntnu.no</a></div>
-                </div>
-                <img class="serviceImage" src="img/proxmox.png" alt="Proxmox-logo">
-              </div>
-
-              <div class="service">
-                <div class="serviceContent">
-                  <h2 class="serviceTitle">Proxmox @buttercup</h2>
-                  <p class="serviceDescription">Buttercup er en av våre VM-tjenere.</p>
-                  <div class="serviceLink"><a href="https://buttercup.pvv.ntnu.no:8006" target="_blank">Gå til buttercup.pvv.ntnu.no</a></div>
-                </div>
-                <img class="serviceImage" src="img/proxmox.png" alt="Proxmox-logo">
-              </div>
-            </div>
-          </div>
-
-          <div class="categoryContainer">
-            <div class="categoryLabel">Underholdning</div>
-            <div class="categoryContent">
-
-              <div class="service">
-                <div class="serviceContent">
-                  <h2 class="serviceTitle">Minecraft</h2>
-                  <p class="serviceDescription">Vi har en egen Minecraft-server <b>for medlemmer</b>, som du kan koble til med IP-adressen <b>minecraft.pvv.ntnu.no</b>. Spør om whitelist på matrix/discord.</p>
-                  <div class="serviceLink"><a href="https://minecraft.pvv.ntnu.no" target="_blank">Gå til verdenskartet vårt</a></div>
-                </div>
-                <img class="serviceImage" src="img/minecraft.png" alt="Minecraft-logo">
-              </div>
-
-              <div class="service">
-                <div class="serviceContent">
-                  <h2 class="serviceTitle">MiniFlux RSS reader</h2>
-                  <p class="serviceDescription">Trenger du en cross-platform RSS/Atom-leser for å følge med på omverdenen som det er 1990? </p>
-                  <div class="serviceLink"><a href="https://feeds.pvv.ntnu.no" target="_blank">Gå til MiniFlux</a></div>
-                </div>
-                <img class="serviceImage" src="img/rss.png" alt="RSS-Ikon">
-              </div>
-
-              <div class="service">
-                <div class="serviceContent">
-                  <h2 class="serviceTitle">Bildegalleri</h2>
-                  <p class="serviceDescription">PVV har et felles bildegalleri, der alle kan legge relevante bilder, som automatisk blir inkludert på nettsiden.</p>
-                  <div class="serviceLink">
-                    <a href="https://www.pvv.ntnu.no/galleri/" target="_blank">Se galleriet</a>
-                  </div>
-                  <div class="serviceLink">
-                    <a href="https://wiki.pvv.ntnu.no/wiki/Bildedeling" target="_blank">Opplasting</a>
-                  </div>
-                </div>
-                <img class="serviceImage" src="img/gallery.png" alt="RSS-Ikon">
-              </div>
-
-              <!-- Bokhylle /brzeczyszczykiewicz ? -->
-
-            </div>
-          </div>
+          <?php endforeach; ?>
         </div>
-      </main>
+      </div>
+      <?php endforeach; ?>
+    </div>
+  </main>
 </body>
 
 </html>
