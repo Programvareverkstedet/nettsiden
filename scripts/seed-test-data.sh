5a25087d32b83587c0e68272a8be5f6e651ec55b
h7x4 <h7x4@nani.wtf>
2026-01-13 18:58:06 +0900

    scripts/seed-test-data: fill some door data

--- scripts/seed-test-data.sh
+++ scripts/seed-test-data.sh
@@ -24,3 +24,25 @@ if [ ! -f "$PROJECT_ROOT/pvv.sqlite" ] ; then
 fi
 
 sqlite3 "$PROJECT_ROOT/pvv.sqlite" < "$PROJECT_ROOT/dist/sql/test_data_sqlite.sql"
+
+# Loop over the last 4 days' unix timestamps in 5-minute intervals and insert test data
+END_TIME=$(date +%s)
+START_TIME=$((END_TIME - 4 * 24 * 60 * 60))
+for ((timestamp=START_TIME; timestamp<=END_TIME; timestamp+=60 * 5 * 10)); do
+  RANDOM_YES_NO=$((RANDOM % 2))
+  sqlite3 "$PROJECT_ROOT/pvv.sqlite" <<EOF
+INSERT INTO
+  door(time, open)
+VALUES
+  ($timestamp + 60 * 5 * 0, $RANDOM_YES_NO),
+  ($timestamp + 60 * 5 * 1, $RANDOM_YES_NO),
+  ($timestamp + 60 * 5 * 2, $RANDOM_YES_NO),
+  ($timestamp + 60 * 5 * 3, $RANDOM_YES_NO),
+  ($timestamp + 60 * 5 * 4, $RANDOM_YES_NO),
+  ($timestamp + 60 * 5 * 5, $RANDOM_YES_NO),
+  ($timestamp + 60 * 5 * 6, $RANDOM_YES_NO),
+  ($timestamp + 60 * 5 * 7, $RANDOM_YES_NO),
+  ($timestamp + 60 * 5 * 8, $RANDOM_YES_NO),
+  ($timestamp + 60 * 5 * 9, $RANDOM_YES_NO);
+EOF
+done
